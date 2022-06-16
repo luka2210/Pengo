b0d60d91149e10dcfa35b2a46b96ddcc9352ae09
luka2210 <lukamomcilovic221099@gmail.com>
2022-06-17 01:09:06 +0200

    podesavanje okruzenja

---
+++ Pengo/Game.cpp
@@ -0,0 +1,93 @@
+#include <math.h>		
+#include <stdio.h>
+#include <stdlib.h>		
+#include "Glut.h"
+#include "Game.h"
+
+
+const double Xmin = 0.0, Xmax = 15.0;
+const double Ymin = 0.0, Ymax = 20.0;
+
+
+void myKeyboardFunc( unsigned char key, int x, int y ) {
+
+}
+
+void mySpecialKeyFunc( int key, int x, int y ) {
+	
+}
+
+void drawScene(void){
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    
+	glMatrixMode(GL_MODELVIEW);		
+	glLoadIdentity();
+
+	glTranslatef(1.0, 1.0, 1.0);
+	glBegin(GL_TRIANGLES);
+	glVertex3f(0.0, 0.0, 0.0);
+	glVertex3f(1.0, 1.0, 0.0);
+	glVertex3f(0.0, 1.0, 0.0);
+	glEnd();
+
+    glFlush();
+    glutSwapBuffers();
+	glutPostRedisplay();
+}
+
+void initRendering() {
+	glShadeModel(GL_FLAT);
+	glEnable(GL_DEPTH);
+}
+
+void resizeWindow(int w, int h) {
+	double scale, center;
+	double windowXmin, windowXmax, windowYmin, windowYmax;
+
+	glViewport( 0, 0, w, h );	
+
+	w = (w==0) ? 1 : w;
+	h = (h==0) ? 1 : h;
+	if ( (Xmax-Xmin)/w < (Ymax-Ymin)/h ) {
+		scale = ((Ymax-Ymin)/h)/((Xmax-Xmin)/w);
+		center = (Xmax+Xmin)/2;
+		windowXmin = center - (center-Xmin)*scale;
+		windowXmax = center + (Xmax-center)*scale;
+		windowYmin = Ymin;
+		windowYmax = Ymax;
+	}
+	else {
+		scale = ((Xmax-Xmin)/w)/((Ymax-Ymin)/h);
+		center = (Ymax+Ymin)/2;
+		windowYmin = center - (center-Ymin)*scale;
+		windowYmax = center + (Ymax-center)*scale;
+		windowXmin = Xmin;
+		windowXmax = Xmax;
+	}
+	
+    glMatrixMode( GL_PROJECTION );
+    glLoadIdentity();
+    glOrtho( windowXmin, windowXmax, windowYmin, windowYmax, -1, 1 );
+}
+
+int main( int argc, char** argv ) {
+	glutInit(&argc,argv);
+ 
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH );
+
+	glutInitWindowPosition(10, 60);
+	glutInitWindowSize(565, 720);
+	glutCreateWindow("Pengo");
+
+    initRendering();
+
+	glutKeyboardFunc(myKeyboardFunc);
+	glutSpecialFunc(mySpecialKeyFunc);
+	glutReshapeFunc(resizeWindow);
+	glutDisplayFunc(drawScene);
+	
+	glutMainLoop();
+
+    return(0);
+}
